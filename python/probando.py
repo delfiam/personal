6d0f75ec3ac2907bc8d828d4ac5ab8ea4d81628d
Delfi <delfinamunich@gmail.com>
2022-05-27 00:38:28 -0300

    error en probando

--- python/probando.py
+++ python/probando.py
@@ -1,3 +1,28 @@
 # print en diferentes tipos 
-lotsofhellos = "hello" * 10
-print(lotsofhellos)
+def holamundo():
+    muchosholas = "hola mundo:)" * 10
+    print(muchosholas)
+
+# upper
+def uppernom():
+    mnombres = ['delfi', 'jere', 'cata']
+    nombremayus = []
+
+    for nombre in mnombres:
+        nombre_ = nombre.upper()
+        nombremayus.append(nombre_)
+        print(nombremayus)
+
+# map
+def dictaduradenombres(dictador):
+    nombre = 'cata'
+    nombre_lista = list('cata')
+    dictador_lista = list(dictador)
+    cambio = map(nombre_lista, dictador_lista)
+    print(cambio) 
+
+holamundo()
+uppernom()
+dictaduradenombres('jerucho')
+
+# https://itsmycode.com/typeerror-list-object-is-not-callable/#:~:text=The%20most%20common%20scenario%20where,parenthesis%20instead%20of%20square%20brackets.
