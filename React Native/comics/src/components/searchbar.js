6401f20c6e612c917300a856496a5104208feb46
Delfina Munich <89984382+delfiam@users.noreply.github.com>
2022-07-03 23:55:55 -0300

    Add files via upload

---
+++ React Native/comics/src/components/searchbar.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import {react, Component} from 'react';
+
+// https://developer.marvel.com/documentation/entity_types
+
+axios({
+    method: 'get',
+    url: ' http://gateway.marvel.com/v1/public/characters?ts=jdklsfjdl&apikey=4973292c7399b338929b4e9148fb35ee&hash=92aae9d46761a9618e02c98e6d987190&',
+    params: {
+      name: 'Deadpool',
+    }
+  }).then(function (response) {
+    console.log(response.data); 
+  });
